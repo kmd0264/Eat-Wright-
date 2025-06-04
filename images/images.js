50a59143f9509db16c23942fbe37faaef5fb68bd
kmdecena0264 <kmd0264@arastudent.ac.nz>
2025-06-04 18:33:20 +1200

    Add files via upload

---
+++ images/images.js
@@ -0,0 +1,41 @@
+// assets/images.js
+
+// Platform & ladder
+export const platformImg = new Image();
+platformImg.src = 'assets/images/tileset/platform.png';
+
+export const ladderImg = new Image();
+ladderImg.src = 'assets/images/tileset/ladder.png';
+
+// Doctor
+export const doctorStandingImg = new Image();
+doctorStandingImg.src = 'assets/images/tileset/doctor_standing.png';
+
+export const doctorThrowingImg = new Image();
+doctorThrowingImg.src = 'assets/images/tileset/doctor_throwing.png';
+
+// Trash
+export const trashCanImg = new Image();
+trashCanImg.src = 'assets/images/tileset/trash.png';
+
+// Golden apple
+export const goldenAppleImg = new Image();
+goldenAppleImg.src = 'assets/images/tileset/golden_apple.png';
+
+// Player images
+export const playerFrontStandingImg = new Image();
+playerFrontStandingImg.src = 'assets/images/wright/front_standing.png';
+
+export const playerLeftStandingImg = new Image();
+playerLeftStandingImg.src = 'assets/images/wright/left_side_standing.png';
+
+export const playerLeftWalkingImgs = [new Image(), new Image()];
+playerLeftWalkingImgs[0].src = 'assets/images/wright/left_side_walking.png';
+playerLeftWalkingImgs[1].src = 'assets/images/wright/left_side_walking_1.png';
+
+export const playerRightStandingImg = new Image();
+playerRightStandingImg.src = 'assets/images/wright/right_side_standing.png';
+
+export const playerRightWalkingImgs = [new Image(), new Image()];
+playerRightWalkingImgs[0].src = 'assets/images/wright/right_side_walking.png';
+playerRightWalkingImgs[1].src = 'assets/images/wright/right_side_walking_1.png';
