1341eab3f58089f6255373c23079686ee3128b54
kmdecena0264 <kmd0264@arastudent.ac.nz>
2025-06-11 20:51:25 +1200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
-// ===============================
-//          ENTRY POINT
-//  Initializes the app when the
-//  DOM is fully loaded
-// ===============================
+// src/main.js
+
+//  This is the app's entry point       
+//  Initializes the app when the DOM is fully loaded
 
 // Import the main menu screen view
 import MainMenuView from './view/MainMenuView.js';
 
-// Wait until the HTML DOM is fully parsed and loaded
+// Wait until the HTML DOM is fully rendered and loaded
 window.addEventListener('DOMContentLoaded', () => {
   // Get the main app container
   const app = document.getElementById('app');
